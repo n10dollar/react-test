e3fcc871db5c2c493863191fc42dd8ae5a37329d
Neil <ntendolkar@berkeley.edu>
2023-05-16 16:10:48 -0700

    conditionals, events, state vars (useState), interface Props for passing in data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import Message from "./components/Message"
 import ListGroup from "./components/ListGroup"
+import {Fragment} from "react";
+
+
+let cities = [
+    "New York",
+    "San Franscisco",
+    "Los Angeles",
+    "Miami"
+];
+
+let cities2 = [
+    "Mumbai",
+    "Chennai",
+    "New Delhi",
+    "Bangalore"
+];
 
 function App() {
-    const htmlCode = (
-        <div>
-            <Message />
-            <ListGroup />
-        </div>
+    // const htmlCode = (
+    //     <div>
+    //         <Message />
+    //         <ListGroup cities={cities} heading={"Cities Heading"}/>
+    //     </div>
+    // );
+    // return htmlCode;
+    return (
+      // <Fragment>
+      <>
+          <Message />
+          <ListGroup cities={cities} heading={"Cities_Heading"} />
+      </>
+      // </Fragment>
     );
-    return htmlCode;
 }
 
 
--- src/components/ListGroup.js
+++ src/components/ListGroup.js
@@ -1,13 +1,44 @@
+import {useState} from "react";
 
-function ListGroup() {
+interface Props {
+    cities: string[];
+    heading: string;
+}
+
+function ListGroup({cities, heading} : Props) {
+
+
+    // cities = [];
+    // let selectedIndex = 0;
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    // return (
+    //     <ul className="list-group">
+    //         <li className="list-group-item">An item</li>
+    //         <li className="list-group-item">A second item</li>
+    //         <li className="list-group-item">A third item</li>
+    //         <li className="list-group-item">A fourth item</li>
+    //         <li className="list-group-item">And a fifth one</li>
+    //     </ul>
+    // );
+
+    const handleClick = event => (
+        console.log(event)
+        // selectedIndex = cli
+    );
+
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        <>
+            <h1 className="h1">{heading}</h1>
+            {cities.length === 0 ? <p>No Cities Found</p> : null}
+            <ul className="list-group">
+                {cities.map((city, index) =>
+                    <li className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+                    onClick={() => setSelectedIndex(index)}>
+                        {city}
+                    </li>)}
+            </ul>
+        </>
     );
 }
 
