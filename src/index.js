b4f20c0ea074412f218f1a58c87f0ab968e05208
Neil <ntendolkar@berkeley.edu>
2023-05-31 17:36:35 -0700

    cleaned up file structure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js'
-import App from './old/AppOld';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
