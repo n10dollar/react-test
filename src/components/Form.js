a38833eac8adfbd0b909ed3ce135aa87497ac5a5
Neil <ntendolkar@berkeley.edu>
2023-05-31 19:35:47 -0700

    input validation via useForm()

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,26 +3,28 @@ import {useForm} from "react-hook-form";
 import {FieldValues} from "react-hook-form";
 
 const Form = () => {
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    console.log(errors);
 
     function handleSubmitDef(data: FieldValues) {
         console.log(data);
     }
 
-    const [person, setPerson] = useState({
-        name: "",
-        age: ""
-    });
-
     return (
         <form onSubmit={handleSubmit(handleSubmitDef)}>
             <div className="mb-3 col-6">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" { ...register("name", { required: true, maxLength: 3}) } type="text" className="form-control"/>
+                <input id="name" { ...register("name", { required: true, minLength: 3}) } type="text" className="form-control"/>
+                {/*{ errors.name?.type === "required" ? <p>REQUIRED!!</p> : null}*/}
+                {/*{ errors.name?.type === "maxLength" ? <p>MAX_LENGTH!!</p> : null}*/}
+                { errors.name?.type ? <p className="text-danger">ERROR: {errors.name.type}</p> : null}
             </div>
             <div className="mb-3 col-6">
                 <label htmlFor="age" className="form-label">Age</label>
                 <input id="age" { ...register("age", { required: true, min: 18 }) } type="number" className="form-control"/>
+                {/*{ errors.age?.type === "required" ? <p>REQUIRED!!</p> : null}*/}
+                {/*{ errors.age?.type === "min" ? <p>MIN!!</p> : null}*/}
+                { errors.age?.type ? <p className="text-danger">ERROR: {errors.age.type}</p> : null}
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
         </form>
