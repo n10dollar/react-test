53a538e6c54b779d6b60eebe663aef2e823193b6
Neil <ntendolkar@berkeley.edu>
2023-05-31 21:08:57 -0700

    input validation via Zod

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,9 +1,18 @@
-import {FormEvent, useRef, useState} from "react";
 import {useForm} from "react-hook-form";
 import {FieldValues} from "react-hook-form";
+import { z } from "zod";
+import { zodResolver} from "@hookform/resolvers/zod";
+
+const schema = z.object({
+    name: z.string().min(3),
+    age: z.number().min(18)
+})
+
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm( { resolver: zodResolver(schema) });
     console.log(errors);
 
     function handleSubmitDef(data: FieldValues) {
@@ -14,17 +23,13 @@ const Form = () => {
         <form onSubmit={handleSubmit(handleSubmitDef)}>
             <div className="mb-3 col-6">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" { ...register("name", { required: true, minLength: 3}) } type="text" className="form-control"/>
-                {/*{ errors.name?.type === "required" ? <p>REQUIRED!!</p> : null}*/}
-                {/*{ errors.name?.type === "maxLength" ? <p>MAX_LENGTH!!</p> : null}*/}
-                { errors.name?.type ? <p className="text-danger">ERROR: {errors.name.type}</p> : null}
+                <input id="name" { ...register("name") } type="text" className="form-control"/>
+                { errors.name ? <p className="text-danger">{errors.name.message}</p> : null}
             </div>
             <div className="mb-3 col-6">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input id="age" { ...register("age", { required: true, min: 18 }) } type="number" className="form-control"/>
-                {/*{ errors.age?.type === "required" ? <p>REQUIRED!!</p> : null}*/}
-                {/*{ errors.age?.type === "min" ? <p>MIN!!</p> : null}*/}
-                { errors.age?.type ? <p className="text-danger">ERROR: {errors.age.type}</p> : null}
+                <input id="age" { ...register("age") } type="number" className="form-control"/>
+                { errors.age ? <p className="text-danger">{errors.age.message}</p> : null}
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
         </form>
