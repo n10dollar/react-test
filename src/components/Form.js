e0ac4cc050327919c61d3d67d08c5c2919a67392
Neil <ntendolkar@berkeley.edu>
2023-05-31 18:46:08 -0700

    register function inputs App.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,28 @@
 import {FormEvent, useRef, useState} from "react";
 import {useForm} from "react-hook-form";
+import {FieldValues} from "react-hook-form";
 
 const Form = () => {
-    const form = useForm();
-    console.log(form);
+    const { register, handleSubmit } = useForm();
+
+    function handleSubmitDef(data: FieldValues) {
+        console.log(data);
+    }
 
     const [person, setPerson] = useState({
         name: "",
         age: ""
     });
 
-    const handleSubmit = (event: FormEvent) => {
-        event.preventDefault();
-        console.log(person.name);
-        console.log(parseInt(person.age));
-        console.log(person);
-    }
-
     return (
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit(handleSubmitDef)}>
             <div className="mb-3 col-6">
                 <label htmlFor="name" className="form-label">Name</label>
-                <input id="name" value={person.name} onChange={(event) => setPerson({ ...person, name: event.target.value })} type="text" className="form-control"/>
+                <input id="name" { ...register("name", { required: true, maxLength: 3}) } type="text" className="form-control"/>
             </div>
             <div className="mb-3 col-6">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input id="age" value={person.age} onChange={(event) => setPerson({ ...person, age: parseInt(event.target.value) })} type="number" className="form-control"/>
+                <input id="age" { ...register("age", { required: true, min: 18 }) } type="number" className="form-control"/>
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
         </form>
